1fc08f1f483959827090db5de1d1fbdbf6fa248d
sanketrakshe <sanketrakshe07@gmail.com>
2023-10-08 03:28:11 +0530

    FEAT: Added isAuthenticated middleware

--- src/middlewares/auth-request-middlewares.js
+++ src/middlewares/auth-request-middlewares.js
@@ -1,6 +1,9 @@
 const { StatusCodes } = require('http-status-codes');
 
 const { ErrorResponse } = require('../utils/common');
+
+const { UserService } = require('../services');
+
 const AppError = require('../utils/errors/app-error');
 
 function validateAuthRequest(req, res, next) {
@@ -21,6 +24,21 @@ function validateAuthRequest(req, res, next) {
     next();
 }
 
+async function checkAuth(req, res, next) {
+    try {
+        const response = await UserService.isAuthenticated(req.headers['x-access-token']);
+        if(response) {
+            req.user = response; // setting the user id in the req object
+            next();
+        }
+    } catch(error) {
+        return res
+                .status(error.statusCode)
+                .json(error);
+    }
+}
+
 module.exports = {
-    validateAuthRequest
+    validateAuthRequest,
+    checkAuth
 }
